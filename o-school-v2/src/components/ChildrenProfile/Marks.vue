febbc4ccde6858f09317a094955d46d476821aa8
Thibault-21 <thib.dec.pro@gamil.com>
2021-02-01 18:41:10 +0100

    [FRONTEND] set Marks component

---
+++ o-school-v2/src/components/ChildrenProfile/Marks.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-data-table></v-data-table>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {},
+})
+export default class Marks extends Vue {
+  public marks = {
+     french: [
+       12, 14
+     ], 
+     sport: [
+       17
+     ]
+    }
+  public headers = [{
+    text: 'betty Obrien'
+  }]   
+}
+</script>
+
+<style>
+
+</style>
